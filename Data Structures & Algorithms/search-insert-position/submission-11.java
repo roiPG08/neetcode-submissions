4d6c4bc742a15235b4ebe12b61ff5c4770ad256d
Przemyslaw Rabczak <87003789+roiPG08@users.noreply.github.com>
2026-09-14 19:26:48 +0200

    Add: search-insert-position - submission-11

---
+++ Data Structures & Algorithms/search-insert-position/submission-11.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int l = 0;
+        int r = nums.length;
+        return search(l, r, nums, target);
+    }
+
+    private int search(int l, int r, int[] nums, int target) {
+        // When bounds cross, 'l' is the correct insertion index
+        if (l >= r) {
+            return l;
+        }
+
+        int mid = l + (r - l) / 2;
+
+        if (nums[mid] >= target) {
+            return search(l, mid, nums, target);
+        } else {
+            return search(mid + 1, r, nums, target);
+        }
+    }
+    // private int search(int l, int r, int[] nums, int target) {
+    //     if (l > r) {
+    //         return l;
+    //     }
+    //     int mid = r + (r - l) / 2;
+
+    //     if (nums[mid] == target) {
+    //         return mid;
+    //     } else if (nums[mid] > target) {
+    //         return search(l, mid - 1, nums, target);
+    //     } else {
+    //         return search(mid + 1, r, nums, target);
+    //     }
+    // }
+}
